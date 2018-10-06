30a7b48d4d2f39f49d74fe46d9cce7ed6af09bea
Your Name <you@example.com>
2018-10-06 16:57:04 +0200

    pierwszy commit

--- zjazd_1/zadanie 10.py
+++ zjazd_1/zadanie 10.py
@@ -3,13 +3,23 @@ print("________________________")
 x = int(input("podaj współrzędną x: "))
 y = int(input("podaj współrzędną y: "))
 
-if x >= 90 and y >= 90:
-    print("Jesteś w prawym górnym rogu planszy")
-elif x <= 10 and y >= 90:
-    print("Jesteś na lewym górnym rogu planszy")
-elif x >= 90 and y <= 10:
-    print("Jesteś na prawym dolnym rogu planszy")
-elif x <= 10 and y <= 10:
-    print("Jesteś na lewym dolnym rogu planszy")
-elif x <= 10 and if (y > 10 and Y < 90):
-    print("Jesteś na lewej krawędzi planszy")
+if x > 100 or y > 100 or x < 0 or y < 0:
+    print("Wypadłeś z planszy")
+elif x > 90 and y > 90:
+    print("Jesteś w prawym górnym rogu")
+elif x > 90 and y < 10:
+    print("Jesteś w prawym dolnym rogu")
+elif x < 10 and y < 10:
+    print("Jesteś w lewym dolnym rogu")
+elif x < 10 and y < 90:
+    print("Jesteś w lewym górnym rogu")
+elif x < 10:
+    print("Jesteś w lewej krawędzi")
+elif x < 90:
+    print("Jesteś na prawej krawędzi")
+elif y < 10:
+    print("Jesteś na dolnej krawędzi")
+elif y > 90:
+    print("Jesteś na górnej krawędzi")
+else:
+    print("Jesteś w centrum")
